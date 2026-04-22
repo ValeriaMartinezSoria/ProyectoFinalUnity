88563248f8de34edcca59e739aba9013acbea0d8
Valeria Martínez <valeriamartinezsoriagalvarro@gmail.com>
2026-04-22 09:44:39 -0400

    Subir cambios

---
+++ ProyectoFinal/Assets/_Scripts/CodeLock3D.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class CodeLock3D : MonoBehaviour
+{
+    public TextMeshPro displayText;
+    public string correctCode = "2580";
+
+    private string input = "";
+
+    public GameObject door;
+
+    public void PressButton(string value)
+    {
+        if (value == "C")
+        {
+            input = "";
+        }
+        else if (value == "E")
+        {
+            CheckCode();
+            return;
+        }
+        else
+        {
+            if (input.Length < 6)
+                input += value;
+        }
+
+        displayText.text = input;
+    }
+
+    void CheckCode()
+    {
+        Debug.Log("Ingresado: " + input);
+        Debug.Log("Correcto: " + correctCode);
+
+        if (input == correctCode)
+        {
+            Debug.Log("Código correcto");
+            OpenDoor();
+        }
+        else
+        {
+            Debug.Log("Código incorrecto");
+            input = "";
+            displayText.text = "";
+        }
+    }
+
+    void OpenDoor()
+    {
+        door.SetActive(false);
+    }
+}
