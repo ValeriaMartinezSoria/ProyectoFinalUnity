fd244f87f101872d0171535474ac121c025691ba
Valeria Martínez <valeriamartinezsoriagalvarro@gmail.com>
2026-04-24 09:16:40 -0400

    Update Nivel1_Aula, MensajeInicial and add nosotras images

--- ProyectoFinal/Assets/_Scripts/MensajeInicial.cs
+++ ProyectoFinal/Assets/_Scripts/MensajeInicial.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using System.Collections;
 
 public class MensajeInicial : MonoBehaviour
 {
-    public GameObject textoMensaje;
+    [Header("Mensajes")]
+    public GameObject mensaje1;
+    public GameObject mensaje2;
+
+    [Header("Tiempos")]
+    public float duracionMensaje1 = 5f;
+    public float tiempoParaMensaje2 = 60f;
+    public float duracionMensaje2 = 5f;
 
     void Start()
     {
-        Invoke("OcultarMensaje", 5f);
+        // Asegurar estado inicial correcto
+        if (mensaje1 != null) mensaje1.SetActive(false);
+        if (mensaje2 != null) mensaje2.SetActive(false);
+
+        StartCoroutine(SecuenciaMensajes());
     }
 
-    void OcultarMensaje()
+    IEnumerator SecuenciaMensajes()
     {
-        textoMensaje.SetActive(false);
+        // ---- MENSAJE 1 ----
+        if (mensaje1 != null)
+        {
+            mensaje1.SetActive(true);
+            yield return new WaitForSeconds(duracionMensaje1);
+            mensaje1.SetActive(false);
+        }
+
+        // Espera antes del segundo mensaje
+        yield return new WaitForSeconds(tiempoParaMensaje2);
+
+        // ---- MENSAJE 2 ----
+        if (mensaje2 != null)
+        {
+            mensaje2.SetActive(true);
+            yield return new WaitForSeconds(duracionMensaje2);
+            mensaje2.SetActive(false);
+        }
     }
 }
