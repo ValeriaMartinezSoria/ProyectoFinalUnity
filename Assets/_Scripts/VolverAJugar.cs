8c5a075dad822e84d2473fc1b184e2be24e148bc
Valeria Martínez <valeriamartinezsoriagalvarro@gmail.com>
2026-04-27 17:56:23 -0400

    Actualizar VolverAJugar, escena All y agregar materiales flecha

--- Assets/_Scripts/VolverAJugar.cs
+++ Assets/_Scripts/VolverAJugar.cs
@@ -3,8 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class VolverAJugar : MonoBehaviour
 {
+    void Start()
+    {
+   
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void ReiniciarJuego()
     {
+      
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
         SceneManager.LoadScene("PrincipalScene");
     }
 }
