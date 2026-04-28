30dabd70c67add8a718b4931cfbc18e234a62510
Valeria Martínez <valeriamartinezsoriagalvarro@gmail.com>
2026-04-27 21:00:29 -0400

    Actualizar JugarAhora.cs, All.unity y configuración de Input Actions

--- Assets/_Scripts/JugarAhora.cs
+++ Assets/_Scripts/JugarAhora.cs
@@ -3,8 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class JugarAhora : MonoBehaviour
 {
+
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void EmpezarJuego()
     {
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
         SceneManager.LoadScene("All");
     }
 }
